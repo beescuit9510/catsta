a8bb007ac651a20a6204a5249c8a27440c4c334c
beescuit <pengsoon20@gmail.com>
2023-12-27 17:19:10 +0900

    fix sidebar and sidebar item to be functional

--- src/components/sidebar-item/sidebar-item.component.tsx
+++ src/components/sidebar-item/sidebar-item.component.tsx
@@ -4,20 +4,22 @@ import { Link as RouterLink } from 'react-router-dom'
 
 export default function SidebarItem({
   Icon,
+  to,
   text,
 }: {
   Icon: React.ReactNode
+  to: string
   text: string
 }) {
   const bg = useColorModeValue('gray.100', 'whiteAlpha.200')
   return (
     <Link
-      display={'flex'}
-      to={'/'}
+      to={to}
       as={RouterLink}
+      display={'flex'}
       alignItems={'center'}
       gap={4}
-      _hover={{ bg: bg }}
+      _hover={{ bg }}
       borderRadius={6}
       p={2}
       w={{ base: 10, lg: 'full' }}
--- src/components/sidebar/sidebar.component.tsx
+++ src/components/sidebar/sidebar.component.tsx
@@ -5,9 +5,12 @@ import { FaSearch } from 'react-icons/fa'
 import { FaRegSquarePlus } from 'react-icons/fa6'
 import { FaRegHeart } from 'react-icons/fa'
 import SidebarItem from '../sidebar-item/sidebar-item.component'
+import { useSignOut } from 'react-firebase-hooks/auth'
+import { auth } from '../../utils/firebase'
 
 export default function Sidebar() {
   const borderColor = useColorModeValue('gray.100', 'whiteAlpha.300')
+  const [logout] = useSignOut(auth)
 
   return (
     <Flex
@@ -29,6 +32,7 @@ export default function Sidebar() {
         paddingY={5}
         paddingX={2}
       >
+        {/* //TODO: refactor heading logo from login, signup and sidebar. */}
         <Heading
           marginRight={'auto'}
           marginLeft={2}
@@ -39,11 +43,23 @@ export default function Sidebar() {
         >
           Catsta
         </Heading>
-        <SidebarItem Icon={<AiFillHome size={20} />} text='Home' />
-        <SidebarItem Icon={<FaSearch size={20} />} text='Search' />
-        <SidebarItem Icon={<FaRegSquarePlus size={20} />} text='Create' />
-        <SidebarItem Icon={<FaRegHeart size={20} />} text='Notification' />
-        <SidebarItem Icon={<Avatar src={''} size={'sm'} />} text='Profile' />
+        <SidebarItem Icon={<AiFillHome size={20} />} to='/' text='Home' />
+        <SidebarItem Icon={<FaSearch size={20} />} to='/' text='Search' />
+        <SidebarItem
+          Icon={<FaRegSquarePlus size={20} />}
+          to='/create'
+          text='Create'
+        />
+        <SidebarItem
+          Icon={<FaRegHeart size={20} />}
+          to='/notification'
+          text='Notification'
+        />
+        <SidebarItem
+          Icon={<Avatar src={''} size={'sm'} />}
+          to='/profile'
+          text='Profile'
+        />
       </Flex>
       <Flex
         direction={'column'}
@@ -51,8 +67,9 @@ export default function Sidebar() {
         gap={15}
         paddingY={5}
         paddingX={2}
+        onClick={logout}
       >
-        <SidebarItem Icon={<BiLogOut size={20} />} text='Logout' />
+        <SidebarItem Icon={<BiLogOut size={20} />} to='/auth' text='Logout' />
       </Flex>
     </Flex>
   )
