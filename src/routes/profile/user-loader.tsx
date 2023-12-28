33da300e8d07a3dfc82307e8adf144d547c0418d
beescuit <pengsoon20@gmail.com>
2023-12-28 19:04:53 +0900

    fix user loader layout

--- src/routes/profile/user-loader.tsx
+++ src/routes/profile/user-loader.tsx
@@ -1,33 +1,47 @@
 import {
   Center,
   Container,
+  Flex,
   Grid,
   Skeleton,
   SkeletonCircle,
-  SkeletonText,
   Stack,
+  VStack,
 } from '@chakra-ui/react'
 
 export default function UserLoader() {
   return (
-    <Center marginTop={20}>
+    <Center marginTop={10}>
       <Container maxW='container.lg'>
+        <Flex
+          gap={{ base: 4, sm: 10 }}
+          py={10}
+          direction={{ base: 'column', md: 'row' }}
+          justifyContent={'center'}
+          alignItems={'center'}
+        >
+          <SkeletonCircle size={{ base: '6rem', md: '8rem' }} />
+
+          <VStack
+            alignItems={{ base: 'center', md: 'flex-start' }}
+            gap={2}
+            mx={'auto'}
+            flex={1}
+          >
+            <Skeleton height='12px' width='150px' />
+            <Skeleton height='12px' width='100px' />
+          </VStack>
+        </Flex>
         <Stack spacing={10}>
-          <SkeletonCircle
-            display={{ base: 'none', md: 'block' }}
-            size={'7rem'}
-          />
-          <Center>
-            <SkeletonCircle
-              display={{ base: 'block', md: 'none' }}
-              size={'7rem'}
-            />
-          </Center>
-          <SkeletonText noOfLines={2} spacing='4' skeletonHeight='2' />
           <Grid templateColumns='repeat(3, 1fr)' gap={3}>
-            {Array(11).fill(
-              <Skeleton height={{ base: '7rem', md: '250px' }} />
-            )}
+            {Array(11)
+              .fill(1)
+              .map((v, idx) => (
+                <Skeleton
+                  key={v + idx}
+                  height={{ base: '7rem', md: '250px' }}
+                />
+              ))}
           </Grid>
         </Stack>
       </Container>
