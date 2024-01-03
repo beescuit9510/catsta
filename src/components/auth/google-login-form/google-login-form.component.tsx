b8c3b0c49d525296c8d0882b2a623dd950a9c81f
beescuit <pengsoon20@gmail.com>
2024-01-03 12:44:53 +0900

    refactor error-handling for google-login form component

--- src/components/auth/google-login-form/google-login-form.component.tsx
+++ src/components/auth/google-login-form/google-login-form.component.tsx
@@ -1,18 +1,23 @@
-import { Button } from '@chakra-ui/react'
+import { Button, FormControl, FormErrorMessage } from '@chakra-ui/react'
 import { FcGoogle } from 'react-icons/fc'
 import useCreateGoogleUser from '../../../hooks/mutations/useCreateGoogleUser'
 
 export default function GoogleLoginForm() {
-  const { mutate, isPending } = useCreateGoogleUser()
+  const { mutate, isPending, isError, error } = useCreateGoogleUser()
 
   return (
-    <Button
-      onClick={() => mutate()}
-      isLoading={isPending}
-      isDisabled={isPending}
-      leftIcon={<FcGoogle />}
-    >
-      Log in with Google
-    </Button>
+    <>
+      <Button
+        onClick={() => mutate()}
+        isLoading={isPending}
+        isDisabled={isPending}
+        leftIcon={<FcGoogle />}
+      >
+        Log in with Google
+      </Button>
+      <FormControl isInvalid={isError}>
+        <FormErrorMessage>{error?.message}</FormErrorMessage>
+      </FormControl>
+    </>
   )
 }
