24829ac922f6417c2b64a92440eae27a9fedc962
beescuit <pengsoon20@gmail.com>
2024-01-04 11:46:17 +0900

    refactor useUser query promise code

--- src/hooks/queries/useUser.ts
+++ src/hooks/queries/useUser.ts
@@ -5,23 +5,20 @@ import { queryClient } from '../../main'
 import { User } from '../../utils/types'
 import { UserKeys } from '../../utils/query-key'
 
-// TODO find firebase + typescript example
 async function user(userId: string): Promise<User> {
-  return getDoc(doc(firestore, 'users', userId))
-    .then((snap) => {
-      if (!snap.exists()) throw new Error('User not found')
-      else return snap
-    })
-    .then((snap) => snap.data() as User)
+  const snapshot = await getDoc(doc(firestore, 'users', userId))
+
+  if (!snapshot.exists()) throw new Error('User not found')
+
+  return snapshot.data() as User
 }
 
 // TODO: return undefined, find out why it returns undefined
 export function useUser(userId: string) {
-  const query = useQuery({
+  return useQuery({
     queryKey: UserKeys.USER(userId),
     queryFn: () => user(userId),
   })
-  return query
 }
 
 export function useCachedUser(userId: string) {
