b665581ad16a66bf6667d53468ba6b0f141ed685
beescuit <pengsoon20@gmail.com>
2024-01-08 02:30:28 +0900

    refactor redundant props for post-image component

--- src/components/common/post-image/post-image.component.tsx
+++ src/components/common/post-image/post-image.component.tsx
@@ -1,16 +1,10 @@
 import { Box, Image, ImageProps } from '@chakra-ui/react'
 
-export default function PostImage({
-  src,
-  ...rest
-}: {
-  src?: string
-} & ImageProps) {
+export default function PostImage({ ...rest }: ImageProps) {
   /* TODO: fix image size */
   return (
     <Box boxSize={'full'}>
       <Image
-        src={src}
         objectFit={'cover'}
         boxSize='full'
         fallbackSrc={'https://placehold.co/600x500?text=...'}
