b5a736b168ad0701f6c1f9e8ab3428931f824c6d
beescuit <pengsoon20@gmail.com>
2024-01-08 20:43:24 +0900

    fix max hieght for suggested users component.

--- src/components/home/suggested-users/suggested-users.component.tsx
+++ src/components/home/suggested-users/suggested-users.component.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Text } from '@chakra-ui/react'
+import { Button, Flex, Stack, Text } from '@chakra-ui/react'
 import UserAvatar from '../../common/user-avatar/user-avatar.component'
 import UserAvatarLoader from '../../common/user-avatar-loader/user-avatar-loader.component'
 import { useIntiniteSuggestedUsers } from '../../../hooks/queries/infinite/useInfiniteSuggestedUsers'
@@ -32,17 +32,19 @@ export default function SuggestedUsers() {
             </Button>
           )}
         </Flex>
-        {data?.pages
-          .flatMap((page) => page.data)
-          .map((user) => (
-            <UserAvatar
-              key={user.id}
-              displayName={user.displayName}
-              userId={user.id}
-              photoURL={user.photoURL}
-              bio={user.bio}
-            />
-          ))}
+        <Stack spacing={4} maxHeight={'500px'} overflowY={'auto'}>
+          {data?.pages
+            .flatMap((page) => page.data)
+            .map((user) => (
+              <UserAvatar
+                key={user.id}
+                displayName={user.displayName}
+                userId={user.id}
+                photoURL={user.photoURL}
+                bio={user.bio}
+              />
+            ))}
+        </Stack>
         {isFetchingNextPage && (
           <UserAvatarLoader length={data!.pages.at(0)!.perPage} />
         )}
