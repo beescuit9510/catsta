7b5ccfacaf9aa15485d93c1a10d8e2f70ae396af
beescuit <pengsoon20@gmail.com>
2024-01-09 18:27:50 +0900

    remove unused file

--- src/utils/constant.ts
+++
@@ -1,8 +0,0 @@
-import { AuthErrorCodes } from 'firebase/auth'
-
-export const ErrorMessage: { [key: string]: string } = {
-  [AuthErrorCodes.INVALID_EMAIL]: 'Given email is not valid',
-  [AuthErrorCodes.EMAIL_EXISTS]: 'Given email is already in use',
-  [AuthErrorCodes.INVALID_LOGIN_CREDENTIALS]:
-    'Given email or password is wrong. Please try again',
-} as const
