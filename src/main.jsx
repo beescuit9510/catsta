85fa168926ae7455b96630112435938299adda1d
beescuit <pengsoon20@gmail.com>
2024-01-10 15:24:51 +0900

    change main.tsx to main.jsx to temporarly fix suspense: true type error

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ export const queryClient = new QueryClient({
   },
 })
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme}>
